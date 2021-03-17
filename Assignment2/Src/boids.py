2fc08284d8d2a765da249fee083fcb78a9c0f339
Lasse Tretten <lassetretten@protonmail.com>
2021-03-17 17:48:02 +0100

    completed first edition. Did not pass dough...

--- Assignment2/Src/boids.py
+++ Assignment2/Src/boids.py
@@ -1,52 +1,32 @@
 """ This is the main program for the second assignment named boids in INF-1400"""
 
-import pygame
-from spriteloader import SpriteMe
-pygame.init()
+import math
 from random import randint, uniform
+from spriteloader import SpriteMe
+import pygame
+pygame.init()
+
 
 
 class Boid(SpriteMe):
-    """ Boids """
+    """ Class representing the boids"""
 
     def __init__(self, surface):
         super().__init__(surface)
         # Velocitry
         self.v = pygame.Vector2()
-        # Acceleration
-        self.a = pygame.Vector2()
 
     def update(self):
-        """ Use the equation of motion (uniform acceleration) to update the position.
+        """ update boid's velocity and possition.
 
         Remark:
         pygame.sprite.Group.update will look for this method.
         It is therofe important to NOT change the name of this method.
         """
-        self.rect.x += self.v.x + (1/2)*self.a.x
-        self.rect.y += self.v.y + (1/2)*self.a.y
+        self.__pixelate_speed__()
 
-    def set_v(self, vx, vy):
-        """ Sets velocity.
-
-        Parameters:
-        -----------
-        vx: Horizontal compunent
-        vy: Vertical component
-        """
-        self.v.x = vx
-        self.v.y = vy
-
-    def set_a(self, ax, ay):
-        """ Sets acceleration.
-
-        Parameters:
-        -----------
-        vx: Horizontal compunent
-        vy: Vertical component
-        """
-        self.a.x = ax
-        self.a.y = ay
+        self.rect.centerx += self.v.x
+        self.rect.centery += self.v.y
 
     def vec_to(self, other):
         """Returns the vector pointing from self to other"""
@@ -55,7 +35,7 @@ class Boid(SpriteMe):
         vec.y = other.rect.centery - self.rect.centery
         return vec
 
-    def close_to(self, other, r = 300):
+    def close_to(self, other, r = 100):
         """ Checks if two boids are close to each other.
 
         Parameters:
@@ -68,6 +48,18 @@ class Boid(SpriteMe):
         if pygame.Vector2.length(self.vec_to(other)) <= r:
             return True
         return False
+
+    def __pixelate_speed__(self):
+        """Prevents small values in velocity to be rounded to zero when update is called"""
+        if self.v.x < 0:
+            self.v.x = math.floor(self.v.x)
+        else:
+            self.v.x = math.ceil(self.v.x)
+
+        if self.v.y < 0:
+            self.v.y = math.floor(self.v.y)
+        else:
+            self.v.y = math.ceil(self.v.y)
 
 
     def neighbours(self, every_boid):
@@ -83,13 +75,29 @@ class Boid(SpriteMe):
         """
         return [boid for boid in every_boid if self.close_to(boid)]
 
+    def check_wall(self, width, height):
+        """Makes is possible for the boids to fly trough the wall.
+
+        Parameters:
+        -----------
+        width: width of the screen
+        height: height of the screen
+        """
+        if self.rect.left <= 0:
+            self.rect.left = width - self.rect.width
+        elif self.rect.right >= width:
+            self.rect.right = self.rect.width
+        elif self.rect.top <= 0:
+            self.rect.top = height - self.rect.widthmag
+        elif self.rect.bottom >= height:
+            self.rect.bottom = self.rect.width
+            
 
 class Flock:
     """Container class for multiple boids."""
     def __init__(self, n_boids, surface):
         self.surface = surface
         self.n_boids = n_boids
-        self.check_IBM()
 
         self.group = pygame.sprite.Group()
         self.every = []
@@ -98,61 +106,105 @@ class Flock:
             self.group.add(boid)
             self.every.append(boid)
 
-    def check_IBM(self):
-        """ Check for some possible IBM errors"""
-        if type(self.n_boids) != int:
-            raise TypeError("Number of boids must be of type int")
-        if self.n_boids < 1:
-            raise ValueError("Number of boids must be greater or equal to 1")
-        if self.n_boids > 1000:
-            raise ValueError("Please, you do not need that many boids!")
-
-    def start_motion(self, pos_random, v_random, speed):
+    def start_motion(self, rp, rv, speed):
         """ Places and sets the boids in motion, with a random component.
 
         Parameters:
         -----------
-        pos_random: random component to the boids possition
-        v_random: random component to the boids velocity direction
-        speed: The boids starting speed
+        rp: random component to the boids possition
+        rv: random component to the boids velocity direction
+        speed: set the boid's speed
         """
         for boid in self.every:
             # Place boids with a random component.
-            boid.rect.center = (SCREEN_WIDTH/2 + randint(-pos_random, pos_random), SCREEN_HEIGHT/2 - randint(-v_random, v_random))
-            boid.v.x = uniform(0, 1)
-            boid.v.y = uniform(0, 1)
-            boid.v = speed*boid.v.normalize()
+            boid.rect.center = (SCREEN_WIDTH/2 + randint(-rp, rp),
+                                SCREEN_HEIGHT/2 - randint(-rv, rv))
+            boid.v.x = uniform(-1, 1)
+            boid.v.y = uniform(-1, 1)
+            boid.v = speed*boid.v
 
-    def alignment(self, boids_nerby):
-        """ Align boids velocities based on it's neighbours"""
+    def alignment(self, boid, neighbours, alignment_factor):
+        """ Align boids velocities based on it's neighbours.
+
+        Parameters:
+        -----------
+        boid: current boid
+        neighbours: boids close to the current boid
+        alignment_factor: determines how strong this effect shall be
+        """
         avrage_direction = pygame.Vector2()
-        for boid in boids_nerby:
-             avrage_direction.x += boid.v.x
-             avrage_direction.y += boid.v.y
+        for neighbour in neighbours:
+            avrage_direction.x += neighbour.v.x
+            avrage_direction.y += neighbour.v.y
 
-        avrage_direction = avrage_direction.normalize()
-        boid.a = 2*avrage_direction
+        avrage_direction = (1/len(neighbours))*avrage_direction
+        boid.v += alignment_factor*(avrage_direction - boid.v)
 
+    def cohesion(self, boid, neighbours, cohesion_factor):
+        """ Boids will try to group togehter. Each boid wannts to steer into the center of the flock.
 
-    def local_update(self):
+        Parameters:
+        -----------
+        boid: current boid
+        neighbours: boids close to the current boid
+        cohesion_factor: determines how strong this effect shall be
+        """
+        avrage_pos = pygame.Vector2()
+        for neighbour in neighbours:
+            avrage_pos.x += neighbour.rect.centerx
+            avrage_pos.y += neighbour.rect.centery
+
+        avrage_pos = (1/len(neighbours))*avrage_pos
+        boid.v += cohesion_factor*(avrage_pos - boid.rect.center - boid.v)
+        boid.v = 10*boid.v.normalize()
+
+    def separation(self, boid, neighbours, separation_factor):
+        """Separate the boids to prevent collisions.
+
+        Parameters:
+        -----------
+        boid: current boid
+        neighbours: boids close to the current boid
+        cohesion_factor: determines how strong this effect shall be
+        """
+        sep_vec = pygame.Vector2()
+        for neighbour in neighbours:
+            sep_vec = -boid.vec_to(neighbour)
+            sep_vec = (1/(sep_vec.length()+ 0.001))*(sep_vec)
+
+        sep_vec = (separation_factor/len(neighbours))*sep_vec
+        boid.v += sep_vec
+
+    def local_update(self, alignment_factor, cohesion_factor, separation_factor):
+        """Updates alignment, cohesion and sepetration.
+
+        Parameters:
+        -----------
+        alignment_factor: determines how strong the alignment effect shall be
+        cohesion_factor: determines how strong the cohesion effect shall be.
+        separation: determines how strong the separation effect shall be.
+        """
         for boid in self.every:
+            boid.check_wall(1400, 1400)
             neighbours = boid.neighbours(self.every)
-            self.alignment(neighbours)
+            if len(neighbours) > 0:
+                self.alignment(boid, neighbours, alignment_factor)
+                self.cohesion(boid, neighbours, cohesion_factor)
+                self.separation(boid, neighbours, separation_factor)
 
-
+            
 if  __name__ == "__main__":
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 800
-    FPS = 10
+    FPS = 30
 
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     boid_surf = pygame.Surface((10, 10))
     boid_surf.fill((255, 0, 255))
-    my_flock = Flock(10, boid_surf)
-    my_flock.start_motion(100, 100, 5)
-    my_flock.local_update()
+    my_flock = Flock(25, boid_surf)
+    my_flock.start_motion(150, 200, 10)
 
     RUNNING = True
     while RUNNING:
@@ -166,14 +218,7 @@ if  __name__ == "__main__":
                 RUNNING = False
 
         screen.fill((0, 0, 0))
-        my_flock.local_update()
+        my_flock.local_update(0.35, 0.03, 15)
         my_flock.group.update()
         my_flock.group.draw(screen)
-
-        # TODO: make this into a method.
-        for boid in my_flock.every:
-            pygame.draw.line(screen, (255, 0, 0), boid.rect.center, (boid.rect.centerx +20* boid.v.x, boid.rect.centery + 20*boid.v.y), width = 2)
-            pygame.draw.line(screen, (0, 255, 0), boid.rect.center, (boid.rect.centerx +20* boid.a.x, boid.rect.centery + 20*boid.a.y), width = 2)
-
-
         pygame.display.flip()
