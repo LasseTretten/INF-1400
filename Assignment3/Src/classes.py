7b3d4be5d74db82198bd91033d216f34e843eb53
Lasse Tretten <lassetretten@protonmail.com>
2021-04-09 19:40:31 +0200

    feat: Change to rotate based steering.

--- Assignment3/Src/classes.py
+++ Assignment3/Src/classes.py
@@ -11,76 +11,95 @@ class SpriteThis(pygame.sprite.Sprite):
     """
     def __init__(self, path, scale = 1):
         super().__init__()
-        surface = pygame.image.load(path)
+        surface = pygame.image.load(path).convert()
         figure = pygame.Surface(surface.get_size())
         figure.set_colorkey((0, 0, 0))
         figure.blit(surface, (0, 0))
-        self.image = pygame.transform.scale(figure, (int(scale*figure.get_width()), int(scale*figure.get_height())))
+        # Orginal Surface
+        self.image_org = pygame.transform.scale(figure, (int(scale*figure.get_width()), int(scale*figure.get_height())))
+        # Makes a copy of the orginal surface.
+        # Instead of rotating the same image multiple times (see method surf_rotate), we overwrite the copy. All transformation is then done on self.image_org.
+        # Multiple transformation on the same image will damage the quality.
+        self.image = self.image_org
         self.rect = self.image.get_rect()
 
-        
+    def surf_rotate(self, deg):
+        center = self.rect.center
+        self.image = pygame.transform.rotate(self.image_org, deg)
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+
+
+
 class Ship(SpriteThis):
     """Base class for the space ships
-    
-    Parameters: 
+
+    Parameters:
     -----------
-    path: path to the image file 
+    path: path to the image file
     scale: scale factor
     """
     def __init__(self, path, scale = 1):
         super().__init__(path, scale)
-        self.v = pygame.Vector2()
-        self.v.xy = 0, 0
+        self.v_unit = pygame.Vector2()
+        self.v_unit.x = 0
+        self.v_unit.y = 1
+        self.speed = 0
+        self.rot = 0
 
-    def friction(self, fx = 0.1, fy = 0.1):
-        """Adds friction to the motion"""
-        if self.v.x > fx:
-            self.v.x -= fx
-        elif self.v.x < -fx:
-            self.v.x += fx
-        else:
-            self.v.x = 0
-    
-        if self.v.y > fy:
-            self.v.y -= fy
-        elif self.v.y < -fy:
-            self.v.y += fy
-        else:
-            self.v.y = 0
-            
+    def friction(self, f = 0.2):
+        """Adds friction to the ships mortion
+        Parameters:
+        -----------
+        f: Friction coefficient
+        """
+        if self.speed < -f:
+            self.speed += f
+        elif self.speed > f:
+            self.speed -= f
 
-    def update(self, v_sense = 0.5 , v_max = 10):
+    def update(self, rot_sense = 4, v_sense = 1.5, v_max = 10):
+        """Updates the possition of the ship according to user input.
+
+        Parameters:
+        -----------
+        rot_sense: Determines how fast the ship rotates
+        v_sense: Determines the ships acceleration
+        v_max: Determines the ships top speed.
+        """
+        if pressed[pygame.K_UP] and self.speed > -v_max:
+            self.speed -= v_sense
+        if pressed[pygame.K_DOWN] and self.speed < v_max/2:
+            self.speed += v_sense/2
+        if pressed[pygame.K_LEFT]:
+            self.v_unit = self.v_unit.rotate(-rot_sense)
+            self.rot += rot_sense
+            self.surf_rotate(self.rot)
+        if pressed[pygame.K_RIGHT]:
+            self.v_unit = self.v_unit.rotate(rot_sense)
+            self.rot -= rot_sense
+            self.surf_rotate(self.rot)
+
         self.friction()
-        self.rect.x += self.v.x
-        self.rect.y += self.v.y
-
-        if pressed[pygame.K_RIGHT] and self.v.x < v_max:
-            self.v.x += v_sense
-        if pressed[pygame.K_LEFT] and self.v.x > -v_max:
-            self.v.x -= v_sense
-        if pressed[pygame.K_DOWN] and self.v.y < v_max:
-            self.v.y += v_sense
-        if pressed[pygame.K_UP] and self.v.y > -v_max:
-            self.v.y -= v_sense
-        
-
-        
-
+        self.rot = self.rot % 360
+        self.v = self.speed*self.v_unit
+        self.rect.centerx += self.v.x
+        self.rect.centery += self.v.y
 
 # This part is only for manual testing.
 if __name__ == "__main__":
-    SCREEN_HEIGHT = 800
-    SCREEN_WIDTH = 1000
+    SCREEN_HEIGHT = 1200
+    SCREEN_WIDTH = 1400
     FPS = 60
 
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     ships = pygame.sprite.Group()
 
-    ship = Ship("../Artwork/PNG/playerShip1_blue.png",)
+    ship = Ship("../Artwork/PNG/playerShip2_green.png",)
     ships.add(ship)
-    
-    
+
+
     # Game loop
     RUNNING  = True
     while RUNNING:
@@ -92,8 +111,8 @@ if __name__ == "__main__":
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_ESCAPE]:
                 RUNNING = False
-                
+
         screen.fill((0, 0, 0))
-        ships.update() 
+        ships.update()
         ships.draw(screen)
         pygame.display.flip()
